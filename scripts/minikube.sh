01992e473eb38266d99f5b89811614b396deb418
Thijs <thijshberg6@gmail.com>
2025-09-01 15:19:10 +0200

    stuff

---
+++ scripts/minikube.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+export ROOT_DIR="$(git rev-parse --show-toplevel)"
+source "$ROOT_DIR/scripts/include/log.sh"
+
+
+function main() {
+    set_log_level
+    minikube start
+    log info "Minikube started"
+}
+
+main "$@"
