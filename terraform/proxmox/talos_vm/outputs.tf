9df707ab976ddd9be974560942469e3819c78a83
Thijs Heijligenberg <thijshberg63@gmail.com>
2025-10-20 13:57:55 +0200

    for_each stuff

--- terraform/proxmox/talos_vm/outputs.tf
+++ terraform/proxmox/talos_vm/outputs.tf
@@ -1,3 +1,3 @@
 output "ip_addresses" {
-    value = [for vm in proxmox_virtual_environment_vm.talos_vm: vm.ipv4_addresses[0]]
+    value = {for vm in proxmox_virtual_environment_vm.talos_vm: "node-${vm.ipv4_addresses}" => vm.ipv4_addresses[0]}
 }
