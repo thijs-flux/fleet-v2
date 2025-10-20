b9abf346d7a5f689f09162834563fe03cdfd5f9b
Thijs Heijligenberg <thijshberg63@gmail.com>
2025-10-20 14:49:30 +0200

    more oops

--- terraform/proxmox/talos_vm/outputs.tf
+++ terraform/proxmox/talos_vm/outputs.tf
@@ -1,3 +1,3 @@
 output "ip_addresses" {
-    value = flatten([for vm in proxmox_virtual_environment_vm.talos_vm: [for addrs in vm: addrs if length(addrs) > 0 && addrs[0] != "127.0.0.1" ]])
+    value = flatten([for vm in proxmox_virtual_environment_vm.talos_vm: [for addrs in vm.ipv4_addresses: addrs if length(addrs) > 0 && addrs[0] != "127.0.0.1" ]])
 }
