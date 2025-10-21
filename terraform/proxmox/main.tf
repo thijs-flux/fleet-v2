b4f0d45053faff1e839b5bca5283ec4012c56923
Thijs Heijligenberg <thijshberg63@gmail.com>
2025-10-21 15:15:22 +0200

    conf

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -139,6 +139,13 @@ resource "cilium" "cluster" {
   depends_on = [ local_file.kubeconfig ]
   set = [
     "ipam.mode=kubernetes",
+    "kubeProxyReplacement=true",
+    "securityContext.capabilities.ciliumAgent={CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}",
+    "securityContext.capabilities.cleanCiliumState={NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}",
+    "cgroup.autoMount.enabled=false",
+    "cgroup.hostRoot=/sys/fs/cgroup",
+    "k8sServiceHost=localhost",
+    "k8sServicePort=7445"
   ]
 }
 
